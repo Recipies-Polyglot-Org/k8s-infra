284ce3e6ed2656ff9a042b84bbd42a0d73d505e4
AKSHAT KAMBOJ <akukamboj@gmail.com>
2025-09-11 09:46:17 +0530

    changed the locks file

--- bootstrap-backend/variables.tf
+++ bootstrap-backend/variables.tf
@@ -6,12 +6,12 @@ variable "region" {
 variable "bucket_name" {
   type        = string
   description = "Unique S3 bucket name for terraform state (must be globally unique)"
-  default     = "akshat-terraform-state"
+  default     = "akshat-recipes-polyglot-tfstate"
 }
 
 variable "dynamodb_table" {
   type    = string
-  default = "akshat-terraform-locks"
+  default = "akshat-recipes-polyglot-locks"
 }
 
 variable "environment" {
